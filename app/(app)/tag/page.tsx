5afc0de1f846edfc4491188a3de163e2793a47dd
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-07-05 00:40:56 +0800

    chore(tag): remove unused useCallback import

--- app/(app)/tag/page.tsx
+++ app/(app)/tag/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useCallback } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 import { PhotoScreen } from './components/photo-screen'
