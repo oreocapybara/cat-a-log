e018091a0a390960594c0e4a147efc821e611216
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-07-07 14:01:05 +0800

    feat(video): assemble DemoVideo root composition with all scenes and transitions

--- video/src/Root.tsx
+++ video/src/Root.tsx
@@ -8,6 +8,7 @@ import {
   TOTAL_DURATION,
   WIPE_DURATION,
 } from './constants/timing'
+import { DemoVideo } from './DemoVideo'
 import { OrangeWipe } from './components/OrangeWipe'
 import { Hook } from './scenes/Hook'
 import { LogoReveal } from './scenes/LogoReveal'
@@ -22,40 +23,20 @@ import { CatchCard } from './scenes/CatchCard'
 import { TechStack } from './scenes/TechStack'
 import { Closing } from './scenes/Closing'
 
-const Placeholder: React.FC = () => (
-  <div
-    style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#FFF7ED',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <p style={{ fontSize: 48, color: '#2A1B12' }}>Scene Placeholder</p>
-  </div>
-)
-
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* Main full-length composition */}
       <Composition
         id="DemoVideo"
-        component={Placeholder}
+        component={DemoVideo}
         durationInFrames={TOTAL_DURATION}
         fps={FPS}
         width={WIDTH}
         height={HEIGHT}
       />
-      <Composition
-        id="OrangeWipe"
-        component={OrangeWipe}
-        durationInFrames={WIPE_DURATION}
-        fps={FPS}
-        width={WIDTH}
-        height={HEIGHT}
-      />
+
+      {/* Individual scene compositions for isolated preview */}
       <Composition
         id="Hook"
         component={Hook}
@@ -148,6 +129,14 @@ export const RemotionRoot: React.FC = () => {
         id="Closing"
         component={Closing}
         durationInFrames={SCENE_DURATIONS.closing}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="OrangeWipe"
+        component={OrangeWipe}
+        durationInFrames={WIPE_DURATION}
         fps={FPS}
         width={WIDTH}
         height={HEIGHT}
